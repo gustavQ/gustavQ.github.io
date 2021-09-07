edd47ff7c4c97d2deb01171a47212d4f0a100258
Gustavo Quintela <quintela.gustavo@gmail.com>
2021-09-07 21:25:56 +0200

    add javascript closemenu function

--- script/static.js
+++ script/static.js
@@ -1,10 +1,17 @@
 const navButton = document.querySelector(".btn-container");
 const navMenu = document.querySelector(".nav-menu");
 const button = document.querySelector(".nav-btn");
+const navLink = document.querySelectorAll(".nav-link");
 
 navButton.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
   button.classList.toggle('show');
   navMenu.classList.toggle('show');
+}
+
+function closeMenu() {
+  button.classList.remove("show");
+  navMenu.classList.remove("show");
 }
